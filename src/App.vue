76510a5576801d09302d802b53403338091edff9
xuxuewen <xxw.code@aliyun.com>
2019-05-01 02:16:50 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="$style.main">
+
+    <!-- 组件A -->
+    <ComA />
+    <hr />
+    <!-- 组件B -->
+    <ComB />
+  </div>
+</template>
+<script>
+import ComA from './ComA.vue';
+import ComB from './ComB.vue';
+export default {
+  name:'App',
+  components:{
+    ComA,
+    ComB
+  },
+  data:function(){
+    return {
+
+    }
+  },
+  mounted:function(){
+    console.log(this.$root.$data)
+  }
+}
+</script>
+<style lang="less" module>
+.main{
+
+}
+</style>
